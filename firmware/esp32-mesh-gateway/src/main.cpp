e8288631ce107a8adbf14d155325d5052f7be4e1
Hieu <kid074334@gmail.com>
2026-01-17 17:57:38 +0700

    UART between stm32 and esp32

--- firmware/esp32-mesh-gateway/src/main.cpp
+++ firmware/esp32-mesh-gateway/src/main.cpp
@@ -2,21 +2,24 @@
 
 #define UART_TX 17
 #define UART_RX 16
+#define baud 9600
 HardwareSerial SerialPort(2);
 void setup()
 {
-  Serial.begin(115200);
-  SerialPort.begin(115200, SERIAL_8N1, UART_RX, UART_TX);
+  Serial.begin(baud);
+  SerialPort.begin(baud, SERIAL_8N1, UART_RX, UART_TX);
 }
 
 void loop()
 {
+  // SerialPort.write(0x10);
+  // delay(1000);
   if (SerialPort.available())
   {
     uint8_t data = SerialPort.read();
 
-    Serial.print("Nhan duoc: 0x");
+    // Serial.print("Nhan duoc: 0x");
     Serial.println(data, HEX);
-    delay(1000);
+    // delay(1000);
   }
 }
