6a17e693869aa5b31309b80fe05e132fdac0e6c5
Hieu <kid074334@gmail.com>
2026-01-18 16:55:20 +0700

    sensor

--- firmware/stm32-sensor-node/Stm32_Sensor_Node/Src/main.c
+++ firmware/stm32-sensor-node/Stm32_Sensor_Node/Src/main.c
@@ -3,18 +3,72 @@
 #include <stm32f401re_rcc.h>
 #include <stm32f401re_usart.h>
 #include <stm32f401re_gpio.h>
+#include <stm32f401re_adc.h>
+#include <timer.h>
 #include <misc.h>
 
 // Định nghĩa chân cho USART1
-#define UART_TX_PIN         GPIO_Pin_9
-#define UART_RX_PIN         GPIO_Pin_10
-#define UART_GPIO_PORT      GPIOA
-#define UART_GPIO_CLOCK     RCC_AHB1Periph_GPIOA
-#define USARTx_INSTANCE     USART1
-#define USARTx_CLOCK        RCC_APB2Periph_USART1 // USART1 nằm trên bus APB2
+#define UART_TX_PIN         	GPIO_Pin_9
+#define UART_RX_PIN         	GPIO_Pin_10
+#define UART_GPIO_PORT      	GPIOA
+#define UART_GPIO_CLOCK     	RCC_AHB1Periph_GPIOA
+#define USARTx_INSTANCE     	USART1
+#define USARTx_CLOCK        	RCC_APB2Periph_USART1 // USART1 nằm trên bus APB2
+// định nghĩa chân sensor
+#define SENSOR_GPIO				GPIOA
+#define SENSOR_GPIO_PIN			GPIO_PinSource1
+#define SENSOR_GPIO_CLOCK     	RCC_AHB1Periph_GPIOA
 
-#define USARTx_Baud         9600
+#define USARTx_Baud         	9600
 
+uint16_t HumiValue;
+uint8_t received_data =0 ;
+uint8_t Send_data = 0x00;
+
+void SENSOR_Init(void){
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_Pin = SENSOR_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_NOPULL;
+
+	RCC_AHB1PeriphClockCmd(SENSOR_GPIO_CLOCK, ENABLE);
+	GPIO_Init(SENSOR_GPIO, &GPIO_InitStructure);
+}
+static void ADC_HUMI_Init(void){
+	ADC_InitTypeDef				ADC_InitStructure;
+	ADC_CommonInitTypeDef		ADC_CommonInitStructure;
+	//Cap clock cho ADC1 hoat dong
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+	// ADC Deinitialization
+	ADC_DeInit(); // khoi tao cac gia trị ADC ve mac dinh
+
+	// ADC Common Init
+	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent; // Moi ADC hoat dong doc lap
+	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div4; // cai dat bo chia tan, quyet dinh xung nhip cho ADC (APB2 = 84Mhz)
+	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled; // khong su dung DMA
+	ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;// chon thoi gian giua 2 lan chuyn doi la 5 Cycles
+	ADC_CommonInit(&ADC_CommonInitStructure);
+
+	// ADC1 Init
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b; // lua chon do phan giai la 12 bit
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE; // cho phep quet che do quet don kenh (1 kenh) va chuyen doi du lieu lien tuc
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE; // chon che do Single Continuous
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfConversion = 1;// thuc hien chuyen doi mot lan trong luc quet
+	ADC_Init(ADC1, &ADC_InitStructure);
+
+
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_144Cycles);
+
+	// Enable ADC1
+	ADC_Cmd(ADC1, ENABLE);
+	ADC_SoftwareStartConv(ADC1);
+}
 void USART1_Init(void) {
     GPIO_InitTypeDef GPIO_InitStructure;
     USART_InitTypeDef USART_InitStructure;
@@ -50,7 +104,34 @@ void USART1_Init(void) {
     // 5. Kích hoạt USART1
     USART_Cmd(USARTx_INSTANCE, ENABLE);
 }
+uint16_t Cover_Humidity(void){
+	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
+	return ADC_GetConversionValue(ADC1);
+}
+static void MultiSensorScan(void)
+{
+    uint32_t dwTimeCurrent;
+    static uint32_t dwTimeTotal, dwTimeInit;
 
+    dwTimeCurrent = GetMilSecTick();
+
+    if (dwTimeCurrent >= dwTimeInit)
+    {
+        dwTimeTotal += dwTimeCurrent - dwTimeInit;
+    }
+    else
+    {
+        dwTimeTotal += 0xFFFFFFFFU - dwTimeCurrent + dwTimeInit;
+    }
+
+    if (dwTimeTotal >= 2000)
+    {
+        // Time scan 2s
+        dwTimeTotal = 0;
+        HumiValue= Cover_Humidity();
+    }
+    dwTimeInit = dwTimeCurrent;
+}
 void Delay_Ms(uint32_t ms){
     uint32_t i,j;
     for(i=0;i<=ms;i++){
@@ -67,13 +148,23 @@ int main(void)
 
     SystemCoreClockUpdate();
     USART1_Init();
+    SENSOR_Init();
+    ADC_HUMI_Init();
 
     while(1) {
+    	Send_data++;
+    	MultiSensorScan();
+    	processTimerScheduler();
+
         // Kiểm tra cờ TXE trước khi gửi để tránh mất bit hoặc rác dữ liệu
         while(USART_GetFlagStatus(USARTx_INSTANCE, USART_FLAG_TXE) == RESET);
 
         // Gửi byte 0x55 (Test)
-        USART_SendData(USARTx_INSTANCE, 0x55);
+        USART_SendData(USARTx_INSTANCE, Send_data);
+
+        while(USART_GetFlagStatus(USARTx_INSTANCE, USART_FLAG_RXNE) == RESET);
+
+        received_data = USART_ReceiveData(USART1);
 
         Delay_Ms(1000);
     }
